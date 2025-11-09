31e8d8497b91da6256cf0a8814ca6d5954cd467b
Marcin Pascua <pascua.marcin1@gmail.com>
2025-11-09 16:25:43 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+import AboutMe from "./components/AboutMe";
+
+export default function App() {
+
+  return (
+    <>
+     <div>
+      <AboutMe />
+     </div>
+
+
+
+    </>
+  )
+}
+
+
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function AboutMe() {
+    return (
+        <div className="header flex flex-row items-center justify-between gap-5">
+            <div className="w-50"><img src="/marcin-pic.png" alt="marcin picture" /></div>
+            <div>Halo</div>
+        </div>
+    );
+}
