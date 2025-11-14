614f0b1f819a5fca5b6bb16adb153334c9dd3349
Marcin Pascua <pascua.marcin1@gmail.com>
2025-11-14 21:24:34 +0800

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,16 +7,16 @@ export default function AboutMe() {
         <img src="/marcin-pic.png" alt="marcin picture" />
       </div>
 
-      <div className="introduction font-poppins text-white space-y-4 w-full sm:w-2/3 leading-none">
-        <h1 className="lg:text-[100px] m-0 sm:text-[50px]">
+      <div className="introduction  mt-28 font-poppins text-white space-y-4 w-full sm:w-2/3 leading-none">
+        <h1 className="lg:text-[130px] m-0 sm:text-[50px]">
           Hello! I'm
-          <span className="lg:text-[150px] sm:text-[90px] text-[#e5eda1] leading-none">
+          <span className="lg:text-[130px] sm:text-[90px] text-[#e5eda1] leading-none">
             {" "}
             Chin
           </span>
         </h1>
 
-        <p className="text-justify text-white/60 text-lg max-w-full sm:max-w-[800px]">
+        <p className="text-justify text-white/60 text-lg max-w-full sm:max-w-[900px]">
           I’m a frontend-focused web development student at the University of
           Cebu – Lapu-Lapu and Mandaue, pursuing a Bachelor’s in Information
           Technology. I love designing clean, responsive, and user-friendly
