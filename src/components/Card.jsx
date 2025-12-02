a21776aa5cf6bc683ac7ab2d5ba3c5d8a2c54d09
Marcin Pascua <pascua.marcin1@gmail.com>
2025-12-02 23:11:59 +0800

    updated css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Card({ logo, title, subtitle, award, year }) {
   return (
-    <div className="card font-poppins w-1/4 mx-auto h-full">
+    <div className="card font-poppins w-full h-full">
       <div className="school-logo flex justify-center items-center mb-2">
         <img className="w-24 h-24" src={logo} alt="school logo" />
       </div>
 
       <div className="content flex flex-col p-3">
-        <h1 className="text-lg font-bold ">{title}</h1>
+        <h1 className="text-xl font-bold ">{title}</h1>
         <h2 className="text-md mt-3 text-justify">{subtitle}</h2>
         <p className="text-md mt-2">{award}</p>
         <p className="italic mt-5 text-sm">{year}</p>
