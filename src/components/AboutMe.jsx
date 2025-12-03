9ceef20ff269915ab537b70dd0eafa247fd8f5d9
Marcin Pascua <pascua.marcin1@gmail.com>
2025-12-03 21:52:42 +0800

    updated AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
           className="aboutme flex flex-col md:flex-row lg:flex-row items-start p-10 gap-5 bg-cover"
           style={{ backgroundImage: "url('/blur effect.png')" }}
         >
-          <div className="chinpic sm:w-full md:w-1/3 lg:w-1/3 flex flex-wrap items-center justify-center">
+          <div className="chinpic sm:w-auto max-w-full md:w-1/3 lg:w-1/3 flex flex-wrap items-center justify-center">
             <img src="/chin-pic.png" alt="chin picture" />
           </div>
 
@@ -22,7 +22,7 @@ export default function AboutMe() {
               </span>
             </h1>
 
-            <p className="intro text-justify text-white/60 text-[30px] md:text-lg  max-w-full sm:max-w-[900px]">
+            <p className="intro text-justify text-white/60 text-[30px] md:text-lg  max-w-full ">
               I’m a frontend-focused web development student at the University
               of Cebu – Lapu-Lapu and Mandaue, pursuing a Bachelor’s in
               Information Technology. I love designing clean, responsive, and
